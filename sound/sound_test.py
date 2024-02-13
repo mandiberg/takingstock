51a839b2304996654b755b733e5e9518fffd3879
Michael Mandiberg <mandiberg@users.noreply.github.com>
2024-02-13 14:53:49 -0500

    sound test output with mixdown (from audition)

--- sound/sound_test.py
+++ sound/sound_test.py
@@ -7,15 +7,18 @@ df = pd.read_csv("sound_files.csv")
 
 # Iterate through each row and play the audio file
 for _, row in df.iterrows():
-    file_path = "sound_files/" + row['file_path']
+    input_path = "sound_files/" + row['file_path']
+    output_path = "sound_output/" + row['file_path']
     volume_drop = .5 - float(row['fit_to_model'])  # Using fit_to_model as the volume level
     pan = float(row['pan'])  # Using fit_to_model as the volume level
-    print("Playing:", file_path, "at volume level:", volume_drop)
-    sound = AudioSegment.from_wav(file_path)
+    print("Playing:", input_path, "at volume level:", volume_drop)
+    sound = AudioSegment.from_wav(input_path)
     
     # Adjusting volume level
     # adjusted_sound = sound - (sound.dBFS - volume_level * sound.dBFS)
-    adjusted_sound = sound - (100 * volume_drop)
-    adjusted_sound = adjusted_sound.pan(pan)  # Adjust to the right
+    adjusted_sound = sound - (95 * volume_drop + 18)
+    adjusted_sound = adjusted_sound.pan(pan)  # Adjust pan
 
-    play(adjusted_sound)
+    # save the adjusted sound
+    adjusted_sound.export(output_path, format="wav")
+    # play(adjusted_sound)
